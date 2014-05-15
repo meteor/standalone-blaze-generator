4d5b861067f9624aa9d916eb96565ff2d9e1dbea
David Greenspan <dgreenspan@alummit.edu>
2014-05-15 14:57:06 -0700

    make $METEOR variable

--- export-packages.sh
+++ export-packages.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
-meteor create app-for-export && cd app-for-export
+
+METEOR=meteor
+
+$METEOR create app-for-export && cd app-for-export
 rm app-for-export.*
-meteor remove standard-app-packages
+$METEOR remove standard-app-packages
 
 for var in "$@"
 do
-  meteor add "$var"
+  $METEOR add "$var"
 done
 
 echo "Bundling..."
-meteor bundle ../bundle.tgz && cd ../ && tar -zxvf bundle.tgz
+$METEOR bundle ../bundle.tgz && cd ../ && tar -zxvf bundle.tgz
 pushd bundle && mv programs/client/*.js ../client.js && popd
 
 rm -rf bundle.tgz
