aa45d44b18176a662a318bd946eedfc8f545e4a3
Slava Kim <slava@meteor.com>
2014-05-25 14:18:14 -0700

    Update the build script to strip the unnecessary comments from linker stage
    reduces the size significantly, these mappings are not that useful anyway

--- build-blaze.sh
+++ build-blaze.sh
@@ -8,3 +8,10 @@ env METEOR_OPTIONS='--debug' ./export-packages.sh templating spacebars-compiler 
 mv client.js blaze.devel.js
 cat blaze-init.js >> blaze.devel.js
 
+# remove trailing '// 123' comments indicating original line numbers
+sed -Eie 's/[[:space:]]*\/\/ [0-9]+$//g' blaze.devel.js
+# remove special comments looking like '// info here ... //'
+sed -Eie '/^\/\/.+\/\/$/d' blaze.devel.js
+# remove special comments for source-map urls
+sed -Eie '/^\/\/# sourceMappingURL=.*/d' blaze.devel.js
+
